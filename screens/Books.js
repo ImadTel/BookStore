aaff5d73b38d7a712845a4cd7a845c7090d125c1
Imad <83423457+ImadTel@users.noreply.github.com>
2024-05-12 21:22:06 +0100

    Update Books.js
    lint

--- screens/Books.js
+++ screens/Books.js
@@ -8,34 +8,22 @@ import Circle from '../assets/images/Circle.svg'
 
 import Icon from 'react-native-vector-icons/MaterialIcons'
 
-
-
 const {height,width} =  Dimensions.get('screen');
 
-
 const Books = () => {
-
     const scrollX = useRef(new Animated.Value(0)).current;
     const Item_Size = width*0.45;
     const Spacer_Size = (width-Item_Size) /2
-
   return (
-       
-       
        <ScrollView      style={{  zIndex:0,  }}>
             <StatusBar  hidden />
             <View style={{height:35,width:"90%",  flexDirection:'row', alignItems:'center',justifyContent:'space-between',
-            marginTop:40,zIndex:0, marginBottom:10 }}> 
-            <Text style ={{color:'white', fontSize:24}}> Our Top Picks  </Text>
-            <TouchableOpacity  >
-                <Text ><Icon name='menu' size={30} color="white" /></Text>
-            </TouchableOpacity>
-       
-       </View> 
-
-
-
-
+                marginTop:40,zIndex:0, marginBottom:10 }}> 
+                <Text style ={{color:'white', fontSize:24}}> Our Top Picks  </Text>
+                <TouchableOpacity  >
+                    <Text ><Icon name='menu' size={30} color="white" /></Text>
+                </TouchableOpacity>
+           </View> 
 
       <Animated.FlatList   
       data={BOOKS}
@@ -54,36 +42,21 @@ const Books = () => {
             }
         )
         
-
         if(!item?.image) return <View style={{width:Spacer_Size,}}></View>
-
          return   <Card book={item} width={Item_Size} translateY={{translateY}}  />
       }}
       horizontal
       showsHorizontalScrollIndicator= {false}
       contentContainerStyle={{paddingTop:50}}
-    
-
       />
-  
-
       <View style={{marginTop:20}}>
         <Text style={{fontSize:24, color:'black',fontWeight:'bold', marginLeft:20,marginVertical:20}}>Bestsellers </Text>
       <BestSeller  />
       </View>
-     
-
-
       <View style={{flex:1,justifyContent:'center',alignItems:'center',alignSelf:'center',top:-180,zIndex:-1,position:'absolute'}}> 
             <Circle   />
         </View>
-     
     </ScrollView>
-
- 
-
-
- 
   )
 }
 
